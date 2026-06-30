1c8bb392bf72efd1988da2bc24d838467a7e4e51
lei.zhang <lei.zhang@ln.ad>
2026-06-30 19:49:43 +0800

    markdown worked

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+// import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://michaelzhang-tech.github.io',
